481c8bf33a688d30669af0b5316be74f36e7e93e
Mathieu Molina <molina.mathieu@hotmail.com>
2022-09-06 09:11:59 +0200

    feat: construct1

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
 class Form{
-    
+
+    private $action;
+    private $method;
+    private $class;
+
+    public function __construct($action, $method, $class){
+        $this->action = $action;
+        $this->method = $method;
+        $this->class = $class;
+    }
 }
