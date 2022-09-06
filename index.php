9353206dedcd9606c47c4f4ca08230baeb87426c
Mathieu Molina <molina.mathieu@hotmail.com>
2022-09-06 09:48:00 +0200

    fix form0.1

--- index.php
+++ index.php
@@ -5,10 +5,26 @@ class Form{
     private $action;
     private $method;
     private $class;
+    private $legend;
+    private $value;
 
-    public function __construct($action, $method, $class){
+    public function entete($action, $method, $class, $legend){
         $this->action = $action;
         $this->method = $method;
         $this->class = $class;
+        $this->legend = $legend;
+
+        return '<form action=" '.  $this->action . '" method=" ' .$this->method . ' " class=" ' . $this->class . ' " >
+                    <fieldset>
+                        <legend> ' . $this->legend . '</legend>';
+    }
+
+    public function setSubmit($value){
+        $this->value = $value;
+
+        return '<input type="submit" value=" ' . $this->value . '">
+                </form>';
     }
 }
+
+$form1 = new Form();
